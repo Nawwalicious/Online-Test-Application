0dbb69c7dcb36872f5e4856cac6173a398c80175
Nawwal Mohammed <102904919+Nawwalicious@users.noreply.github.com>
2022-10-29 15:27:28 +0530

    Add files via upload

---
+++ Online Test Application Front End Stack/js/data.js
@@ -0,0 +1,37 @@
+var jsonData = [
+    {
+        "q": "CSS stands for ?",
+        "opt1" : "Cascading Style Sheets",
+        "opt2" : "Custom Saved Styles",
+        "opt3" : "All of these",
+        "answer": "Cascading Style Sheets"
+    },
+    {
+        "q" : "HTML stands for?",
+        "opt1" : "Hyper Text Markup Language",
+        "opt2" : "High Text Markup Language",
+        "opt3" : "Hyper Tabular Markup Language",
+        "answer" : "Hyper Text Markup Language"
+    },
+    {
+        "q" : "Which of the following tag is used to mark a begining of paragraph ?",
+        "opt1" : "<td>",
+        "opt2" : "<br>",
+        "opt3" : "<p>",
+        "answer" : "<p>"
+    },
+    {
+        "q" : "From which tag descriptive list starts ?",
+        "opt1" : "<ll>",
+        "opt2" : "<dl>",
+        "opt3" : "<dd>",
+        "answer" : "<dl>"
+    },
+    {
+        "q" : "Correct HTML tag for the largest heading is _____",
+        "opt1" : "<h1>",
+        "opt2" : "<h6>",
+        "opt3" : "heading",
+        "answer" : "<h1>"
+    }
+];
